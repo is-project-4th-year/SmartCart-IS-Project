cd09429b8880ca390daf01661d94663d0e59a4c0
Perismakworo <peris.makworo@strathmore.edu>
2025-11-18 15:55:47 +0300

    chore: sync tracked files

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/perismakworo/Downloads/v4/venv"
+set -gx VIRTUAL_ENV "/Users/perismakworo/Downloads/SmartCart Project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
